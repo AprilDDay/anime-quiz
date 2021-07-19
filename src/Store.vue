73871f7343faebbebeb8627fa838f98347294cd5
April Day <april.day@gmail.com>
2021-07-19 18:46:28 +0100

    add shuffle answer to quiz component select header commit

--- src/Store.vue
+++ src/Store.vue
@@ -1,22 +1,53 @@
 <script>
+
+//not sure the store goes here
 import Vue from 'vue'
     import Vuex from 'vuex'
+
+import _ from 'lodash'
 
     Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        
+        Question: [],
+        current: 0,
         difficultyLevel: null,
+        shuffledAnswers: [],
+        selectedIndex: null,
+        correctIndex: null,
+        correctAnswers: 0,
+        answered: false,
+
     }, 
     mutations: {
         SET_DIFFICULTY (state, difficulty) {
             state.difficultyLevel = difficulty
+        },
+        SET_QUESTION (state, data) {
+            state.Question = data
+        },
+        SHUFFLED_ANSWER(state) {
+            var Options;
+            Options = _.concat(
+                state.Question[state.current].incorrect_answers,
+                state.Question[state.current].correct_answer
+            );
+            state.shuffledAnswers = _.shuffle(Options);
+            console.log(state.shuffledAnswers);
         }
     }, 
     actions: {
-        beginQuiz:  ({commit}, difficulty) => {
+        beginQuiz:  ({ commit, state }, difficulty) => {
             commit('SET_DIFFICULTY', difficulty)
+            fetch(`https://opentdb.comp/api.php?amount=10&category=31&difficulty=${state.difficultyLevel}&type=multiple`)
+                .then(response => response.json())
+                .then(data=> {
+                    commit("SET_QUESTION", data.results);
+                    commit("SHUFFLED_ANSWER");
+                    console.log(state.Question)
+                })
+                .catch(error => console.log("Error: ", error))
         }
     },
 })
--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -1,5 +1,63 @@
 
+<template>
+    <div>
+        <div class="container">
+            <div v-if="Question.length">
+                <h1><span>Question: {{current +1}}</span></h1>
+                <h2>{{ Question[current].question }}</h2>
+                <div class="options">
+                    <ul v-for="(options, index) in shuffledAnswers" :key="index">
+                        <li @click="selectedAnswer(index)">{{ options }}</li>
+                    </ul>
+                </div>
+                <div class="nav">
+                    <button>submit</button>
+                    <button @click="next">next</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
 <script>
-    //probably need to fetch the questions here
-    //fetch(`https://opentdb.com/api.php?amount=10&category=31&difficulty=${state.difficultyLevel}&type=multiple`)
+    import {mapState} from 'vuex'
+
+    export default {
+        methods: {
+            ...mapAction(['next'])
+        },
+        computed: {
+            ...mapState(['Question', 'current']),
+        },
+        mutations: {
+            NEXT (state) {
+                state.current++
+            },
+            SELECTED_ANSWER (state, index) {
+                console.log(index)
+                state.selectedIndex = index
+            },
+            SHUFFLE_ANSWER (state) {
+                var Options 
+                Options = _.concat(state.Question[state.current].incorrect_answers, state.Question[state.current].correct_answer)
+                state.shuffledAnswers = _.shuffle(Options)
+                state.correctIndex = state.shuffledAnswers.indexOf(state.Question[state.current].correct_answer)
+            },
+        },
+        actions: {
+            next: ({commit}) => {
+                commit('NEXT')
+            }, 
+            selectedAnswwer: ({ commit}, index) => {
+                commit('SELECTED_ANSWER', index)
+            }
+        },
+        watch: {
+            current: {
+                handler() {
+                    this.$store.commit('SHUFFLE_ANSWER')
+                }
+            }
+        }
+    }
 </script>
